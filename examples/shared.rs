652419e540ffd9e6a291a0edf7ced3ec5cce48d5
schmidtadrian <schmidt.adrian@online.de>
2022-07-05 23:43:05 +0200

    upgrage examples to bevy 0.7

--- examples/shared.rs
+++ examples/shared.rs
@@ -41,16 +41,16 @@ impl ClientMessage for NewChatMessage {
 }
 
 #[allow(unused)]
-pub fn client_register_network_messages(app: &mut AppBuilder) {
+pub fn client_register_network_messages(app: &mut App) {
     use bevy_spicy_networking::AppNetworkClientMessage;
 
     // The client registers messages that arrives from the server, so that
     // it is prepared to handle them. Otherwise, an error occurs.
-    app.listen_for_client_message::<NewChatMessage>();
+    &app.listen_for_client_message::<NewChatMessage>();
 }
 
 #[allow(unused)]
-pub fn server_register_network_messages(app: &mut AppBuilder) {
+pub fn server_register_network_messages(app: &mut App) {
     use bevy_spicy_networking::AppNetworkServerMessage;
 
     // The server registers messages that arrives from a client, so that
